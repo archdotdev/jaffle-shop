32a8fa435b989f307c8632c6ca477dc3c694fad1
Pat Nadolny <patnadolny@gmail.com>
2024-07-12 15:59:15 -0400

    fix generate schema

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,9 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {% if custom_schema_name %}
-        {%- set new_schema_name = custom_schema_name -%}
+        {{ custom_schema_name | trim }}
     {% else %}
-        {%- set new_schema_name = target.schema -%}
+        {{ target.schema }}
     {% endif %}
 
 {%- endmacro %}
